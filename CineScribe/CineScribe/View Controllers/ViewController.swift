d0d4f69c66e6d7f59cb3f4589695b3943bf91259
Marlon <marmoneymar@icloud.com>
2019-09-23 11:00:27 -0400

    Setup project so we're ready to go and have a good jumping off point
    Most storyboards have been created & most folders have been created

---
+++ CineScribe/CineScribe/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CineScribe
+//
+//  Created by Marlon Raskin on 9/23/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
