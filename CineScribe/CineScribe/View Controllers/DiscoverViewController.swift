d6089ba4227011e3396b0794d5991b2bed513876
Marlon <marmoneymar@icloud.com>
2019-09-23 19:38:22 -0400

    Adding more views for acknowledgments & search

---
+++ CineScribe/CineScribe/View Controllers/DiscoverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverViewController.swift
+//  CineScribe
+//
+//  Created by Marlon Raskin on 9/23/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
