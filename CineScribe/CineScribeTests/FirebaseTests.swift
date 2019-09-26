400113bc518ffc00501fe6935b86ecbfaa3d46f1
Jeffrey <SantanaJny@gmail.com>
2019-09-26 19:23:20 -0400

    Adding unit testing

---
+++ CineScribe/CineScribeTests/FirebaseTests.swift
@@ -0,0 +1,26 @@
+//
+//  FirebaseTests.swift
+//  CineScribeTests
+//
+//  Created by Jeffrey Santana on 9/26/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import XCTest
+import CineScribe
+import Firebase
+
+class FirebaseTests: XCTestCase {
+	
+	var user: User?
+
+    override func setUp() {
+//		user = User
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
