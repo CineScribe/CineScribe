2b6db5600954214f68493ba56c514a30fcaea419
Marlon <marmoneymar@icloud.com>
2019-09-26 18:11:01 -0400

    Created VC's for search while creating a note, resized tmdb logo
    Changed title for search tableVC when creating a new note

---
+++ CineScribe/CineScribe/Views/NoteMovieSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteMovieSearchTableViewCell.swift
+//  CineScribe
+//
+//  Created by Marlon Raskin on 9/26/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class NoteMovieSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
